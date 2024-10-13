94df7040ec276318ba156c7c831ff73f8791c16b
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2024-10-13 07:07:28 +0000

    update

--- bot/plugins/settings.py
+++ bot/plugins/settings.py
@@ -6,7 +6,7 @@ from bot.core import filters as fltr
 from bot.core.utils import generate_keyboard
 from bot.core import database as db
 
-@Client.on_message(fltr.cmd(["settings"]))
+#@Client.on_message(fltr.cmd(["settings"]))
 async def user_settings(client, message):
     user = await db.get_user(message.from_user.id)
     use_credits = user.settings.get("use_credits", False)
@@ -16,7 +16,7 @@ async def user_settings(client, message):
     await message.reply(text, reply_markup=generate_keyboard(btn))
 
 
-@Client.on_callback_query(fltr.on_marker("credits"))
+#@Client.on_callback_query(fltr.on_marker("credits"))
 async def toggle_credits(client, query):
    d = query.data.split("_")[-1]
    if d == "on":
