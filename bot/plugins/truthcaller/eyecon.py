dd869b3c82d754d1238cec8d68aadcbc6b446e7c
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2025-05-12 14:43:26 +0530

    Update eyecon.py

--- bot/plugins/truthcaller/eyecon.py
+++ bot/plugins/truthcaller/eyecon.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 def eyecon_search(num):
   url = f"https://api.eyecon-app.com/app/getnames.jsp?cli={num}&lang=en&is_callerid=true&is_ic=true&cv=vc_510_vn_4.0.510_a&requestApi=URLconnection&source=MenifaFragment"
@@ -7,10 +8,10 @@ def eyecon_search(num):
       "Accept": "application/json",
       "Accept-Encoding": "gzip",
       "Connection": "Keep-Alive",
-      "e-auth-k": "PgdtSBeR0MumR7fO",
-      "e-auth-c": "33",
-      "e-auth-v": "e1",
-      "e-auth": "3e3495d4-f9c0-4935-80db-35979253968d",
+      "e-auth-k": os.environ['e-auth-k'],
+      "e-auth-c": os.environ['e-auth-c'],
+      "e-auth-v": os.environ['e-auth-v'],
+      "e-auth": os.environ['e-auth']",
       "content-type": "application/x-www-form-urlencoded",
       "Host": "api.eyecon-app.com"
   }
